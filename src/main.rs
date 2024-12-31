2978fbd91ccdd64addb95be9a10c5f68c360b298
SaiSawant1 <saisawant2003@gmail.com>
2024-12-31 21:38:33 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{fs::File, io::Write, net::TcpListener};
+
+use bytes::Bytes;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,8 +10,10 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream
+                    .write_all("+PONG\r\n".as_bytes())
+                    .expect("Failed write to client");
             }
             Err(e) => {
                 println!("error: {}", e);
